545458345e9939dcf79992818ba3ae24448dfc96
Neimaad <damienlaoussing@hotmail.com>
2022-11-13 02:11:24 +0100

    small modification

--- c-sharp_text_realtime_game/Interfaces/IPoisoning.cs
+++ c-sharp_text_realtime_game/Interfaces/IPoisoning.cs
@@ -7,7 +7,7 @@ namespace c_sharp_text_realtime_game.Interfaces
         public void DealPoisonDamage(Character target, int damageDeal, double rate)
         {
             int poisonDamage = (int)(damageDeal * rate);
-            Console.WriteLine("{0} : empoisone {1}", this.Name, target.Name);
+            Console.WriteLine("{0} empoisone {1}", this.Name, target.Name);
             Console.WriteLine("{0} : -{1} PDV", target.Name, poisonDamage);
 
             target.PoisonDamages.Add(poisonDamage);
